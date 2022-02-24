d5de8ca1b6c51bb6ecc47c2aeea6844c8085cee4
EloySPAIN <yole.tres@gmail.com>
2022-02-24 21:07:00 +0100

    Arduino

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import serial
 arduino = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
-    comando = input('Introduce un comando: ')
+    comando = input('Introduce un comando:  ')
 
     arduino.write(comando.encode())
