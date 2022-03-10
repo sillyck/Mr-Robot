71d0b6f2cadd6689cb1ac471874078dc2464a8c4
EloySPAIN <yole.tres@gmail.com>
2022-03-10 17:59:48 +0100

    Arduino

--- main.py
+++ main.py
@@ -1,36 +1,29 @@
 import serial
-led=0
-variable= False
-arduino = serial.Serial('/dev/ttyACM0', 9600)
-def show():
-    if led==1:
-        print('****************'
-              'Opciones:'
-              '****************'
-              'W = Mover hacia delante'
-              'D = Rotar a la derecha'
-              'S = Mover hacia atras'
-              'A = Rotar a la izquierda')
-    else:
-        print('****************'
-              'L = Encender led'
-              'X = Salir')
-while True:
-    show();
-    comando = input('Introduce un comando:  ')
-    arduino.write(comando.encode())
-    while variable != True:
-        if comando == 'X':
-            print("Saliendo")
-            variable= True
-        elif comando == 'W':
-            print("Moviendose hacia delante")
-        elif comando == 'S':
-            print("Moviendose hacia atras")
-        elif comando == 'D':
-            print("Rotando hacia la derecha")
-        elif comando == 'A':
-            print("Rotando hacia la izquierda")
-        elif comando == 'L':
-            print("Led encendida")
-        arduino.close();
+
+robot = serial.Serial('/dev/ttyACM0', 115200)
+
+
+def main(letra):
+    while True:
+        key = input('Introduce un comando:  ')
+        robot.write(key.encode())
+        # print key
+        if key == 113:
+            break
+        elif key == 'W':
+            print("adelante")
+            cmd_vel[0.1, 0.1, 0.5]
+        elif key == 97:
+            print("izquierda")
+
+        elif key == 115:
+            print("atras")
+
+        elif key == 100:
+            print("derecha")
+
+        else:
+            print("detenido")
+
+
+robot.close();
