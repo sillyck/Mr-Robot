bccc5e320191b759b91dcde83acb48193fff5226
EloySPAIN <yole.tres@gmail.com>
2022-03-11 19:59:56 +0100

    Robot

--- main.py
+++ main.py
@@ -1,29 +1,20 @@
 import serial
-
-robot = serial.Serial('/dev/ttyACM0', 115200)
 comando = 'cmd_vel[0.1,0,0]'
-
-
-def functionMover():
-    return comando
-
+robot = serial.Serial('/dev/ttyACM0', 115200)
 
 while True:
+    
     key = input('Introduce un comando:  ')
     # print key
-    if key == 113:
-        break
-    elif key == 'W':
+    if key == 'W':
         print("adelante")
-        robot.write(functionMover().encode())
-    elif key == 97:
-        print("izquierda")
-
-    elif key == 115:
-        print("atras")
-
-    elif key == 100:
-        print("derecha")
-
+        robot.write(comando.encode())
+        robot.write('\n'.encode())
+        
+        variable = robot.readline()
+        print (variable)
+        # say what you got:
     else:
         print("detenido")
+        robot.close()
+        break
