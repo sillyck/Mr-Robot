9c8f5e55e0ecd78cff8a2a97235cfbc44f28dc46
EloySPAIN <yole.tres@gmail.com>
2022-03-21 20:15:24 +0100

    Pyhton Robot

---
+++ interfaz.py
@@ -0,0 +1,22 @@
+from cgitb import text
+from tkinter import * # Importamos libreria 
+
+ventana = Tk()
+ventana.title('Controles')
+ventana.geometry('600x500')
+ventana.resizable(False, False)
+lb = Label(text= 'W => Adelante', fg = 'blue', font = ('Liberation Serif', 8), width = 20, height = 2).place(x=10,y=0)
+lb2 = Label(text= 'S => Atras', fg = 'blue', font = ('Liberation Serif', 8), width = 20, height = 2).place(x=0,y=50)
+lb3 = Label(text= 'A => Izquierda', fg = 'blue', font = ('Liberation Serif', 8), width = 20, height = 2).place(x=9,y=100)
+lb4 = Label(text= 'D => Derecha', fg = 'blue', font = ('Liberation Serif', 8), width = 20, height = 2).place(x=7,y=150)
+lb5 = Label(text= 'E => Rotación derecha', fg = 'blue', font = ('Liberation Serif', 8), width = 20, height = 2).place(x=300,y=0)
+lb6 = Label(text= 'Q => Rotación izqueirda', fg = 'blue', font = ('Liberation Serif', 8), width = 20, height = 2).place(x=303,y=50)
+
+teclaW = Button(text = "W", width = 5, height = 2).place(x = 250, y = 300)
+teclaS = Button(text = "S", width = 5, height = 2).place(x = 250, y = 350)
+teclaD = Button(text = "D", width = 5, height = 2).place(x = 300, y = 350)
+teclaA = Button(text = "A", width = 5, height = 2).place(x = 200, y = 350)
+teclaE = Button(text = "E", width = 5, height = 2).place(x = 300, y = 300)
+teclaQ = Button(text = "Q", width = 5, height = 2).place(x = 200, y = 300)
+
+ventana.mainloop()
--- main.py
+++ main.py
@@ -9,60 +9,59 @@ rot_der = 'cmd_vel[0,0,0.5]'
 rot_iz = 'cmd_vel[0,0,-0.5]'
 # Conexión a arduino
 robot = serial.Serial('/dev/ttyACM0', 115200)
+def movimiento(k):
+    while True:
+        key = input('Introduce un comando:  ')
+        # print keyW
+        if key == 'W' or key == 'w' or k == 'W' or k == 'w':
+            print("adelante")
+            # Envia al arduino el comando cmd_vel
+            robot.write(delante.encode())
+            robot.write('\n'.encode()) #Ponemos \n (asignamos nueva linia para que no se junte con el siguiente comando y no de error)
+            # Recibimos la inforación del arduino y lo imprimimos por pantalla
+            variable = robot.readline()
+            print (variable)
+        elif key == 'S' or key == 's':
+            print("atras")
 
-while True:
-    
-    key = input('Introduce un comando:  ')
-    # print keyW
-    if key == 'W' or key == 'w':
-        print("adelante")
-        # Envia al arduino el comando cmd_vel
-        robot.write(delante.encode())
-        robot.write('\n'.encode()) #Ponemos \n (asignamos nueva linia para que no se junte con el siguiente comando y no de error)
-        # Recibimos la inforación del arduino y lo imprimimos por pantalla
-        variable = robot.readline()
-        print (variable)
-    elif key == 'S' or key == 's':
-        print("atras")
+            robot.write(atras.encode())
+            robot.write('\n'.encode()) 
 
-        robot.write(atras.encode())
-        robot.write('\n'.encode()) 
+            variable = robot.readline()
+            print (variable)
+        elif key == 'D' or key == 'd':
+            print("derecha")
 
-        variable = robot.readline()
-        print (variable)
-    elif key == 'D' or key == 'd':
-        print("derecha")
+            robot.write(derecha.encode())
+            robot.write('\n'.encode()) 
 
-        robot.write(derecha.encode())
-        robot.write('\n'.encode()) 
+            variable = robot.readline()
+            print (variable)
+        elif key == 'A' or key == 'a':
+            print("izquierda")
 
-        variable = robot.readline()
-        print (variable)
-    elif key == 'A' or key == 'a':
-        print("izquierda")
+            robot.write(izquierda.encode())
+            robot.write('\n'.encode()) 
 
-        robot.write(izquierda.encode())
-        robot.write('\n'.encode()) 
+            variable = robot.readline()
+            print (variable)
+        elif key == 'E' or key == 'e':
+            print("Rotando hacia la derecha")
 
-        variable = robot.readline()
-        print (variable)
-    elif key == 'E' or key == 'e':
-        print("Rotando hacia la derecha")
+            robot.write(rot_der.encode())
+            robot.write('\n'.encode()) 
 
-        robot.write(rot_der.encode())
-        robot.write('\n'.encode()) 
+            variable = robot.readline()
+            print (variable)
+        elif key == 'Q' or key == 'q':
+            print("Rotando hacia la izquierda")
 
-        variable = robot.readline()
-        print (variable)
-    elif key == 'Q' or key == 'q':
-        print("Rotando hacia la izquierda")
+            robot.write(rot_iz.encode())
+            robot.write('\n'.encode()) 
 
-        robot.write(rot_iz.encode())
-        robot.write('\n'.encode()) 
-
-        variable = robot.readline()
-        print (variable)
-    else:
-        print("detenido")
-        robot.close()
-        break
+            variable = robot.readline()
+            print (variable)
+        else:
+            print("detenido")
+            robot.close()
+            break
