1bcb6e75f0c27f48c7bf65ca72f0872ec4f71999
Jordi Ribellas <ribellas2002@gmail.com>
2022-04-05 21:12:19 +0200

    Interficie Grafica

--- interfaz.py
+++ interfaz.py
@@ -1,23 +1,21 @@
-import main
 from cgitb import text
-from tkinter import * # Importamos libreria 
+from tkinter import *
+from tkinter import font
 
-ventana = Tk()
-ventana.title('Controles')
-ventana.geometry('600x500')
-ventana.resizable(False, False)
-lb = Label(text= 'W => Adelante', fg = 'blue', font = ('Liberation Serif', 8), width = 20, height = 2).place(x=10,y=0)
-lb2 = Label(text= 'S => Atras', fg = 'blue', font = ('Liberation Serif', 8), width = 20, height = 2).place(x=0,y=50)
-lb3 = Label(text= 'A => Izquierda', fg = 'blue', font = ('Liberation Serif', 8), width = 20, height = 2).place(x=9,y=100)
-lb4 = Label(text= 'D => Derecha', fg = 'blue', font = ('Liberation Serif', 8), width = 20, height = 2).place(x=7,y=150)
-lb5 = Label(text= 'E => Rotación derecha', fg = 'blue', font = ('Liberation Serif', 8), width = 20, height = 2).place(x=300,y=0)
-lb6 = Label(text= 'Q => Rotación izqueirda', fg = 'blue', font = ('Liberation Serif', 8), width = 20, height = 2).place(x=303,y=50)
+robot = Tk()
 
-teclaW = Button(text = "W", width = 5, height = 2, command=main.movimiento('W')).place(x = 250, y = 300)
-teclaS = Button(text = "S", width = 5, height = 2, command=main.movimiento('S')).place(x = 250, y = 300)
-teclaD = Button(text = "D", width = 5, height = 2, command=main.movimiento('D')).place(x = 300, y = 300)
-teclaA = Button(text = "A", width = 5, height = 2, command=main.movimiento('A')).place(x = 200, y = 300)
-teclaE = Button(text = "E", width = 5, height = 2, command=main.movimiento('E')).place(x = 300, y = 300)
-teclaQ = Button(text = "Q", width = 5, height = 2, command=main.movimiento('Q')).place(x = 200, y = 300)
+app_width = 1000
+app_height = 500
 
-ventana.mainloop()
+screen_width = robot.winfo_screenwidth()
+screen_height = robot.winfo_screenheight()
+
+robot.geometry(f'{app_width}x{app_height}+{100}+{100}')
+
+botonPIzquierda=Button(text="Manual", width=10, height=5)
+botonPDerecha=Button(text="Automatico", width=10, height=5)
+
+botonPIzquierda.grid(row=3, column=0)
+botonPDerecha.grid(row=3, column=4)
+
+robot.mainloop()
