dfe8688b6f253019252823b560d25d2c07e00eb4
EloySPAIN <yole.tres@gmail.com>
2022-04-07 22:12:35 +0200

    main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from time import sleep
 from tkinter import W
 import serial
 # Variables, velocidades del robot
@@ -12,25 +13,6 @@ cont = 0
 # Conexión a arduino
 robot = serial.Serial('/dev/ttyACM0', 115200)
 
-# def funcion_Sensor():
-#     robot.write(sensor.encode())
-#     robot.write('\n'.encode())
-#     # Funcion para quitar mensajes.
-#     if cont == 0:
-#         variable = robot.readline()
-#         variable = robot.readline()
-#         cont=1
-#     if cont == 2:
-#         variable = robot.readline()
-#         cont = 1 
-#     if cont == 1:
-#         variable = robot.readline()
-#         cont = 2
-#     varsens = variable.decode("utf-8")
-#     # archivo = open("sensores.txt", "r+")
-#     # archivo.write(varsens)
-#     # archivo.read()
-#     print(varsens)   
 
 while True:
     key = input('Introduce un comando:  ')
@@ -40,66 +22,56 @@ while True:
         # Envia al arduino el comando cmd_vel
         robot.write(delante.encode())
         robot.write('\n'.encode()) #Ponemos \n (asignamos nueva linia para que no se junte con el siguiente comando y no de error)
-        # Recibimos la inforación del arduino y lo imprimimos por pantalla
-        robot.write(sensor.encode())
-        robot.write('\n'.encode())
-        # Funcion para quitar mensajes.
-        if cont == 0:
-            variable = robot.readline()
-            variable = robot.readline()
-            cont=1
-        if cont == 2:
-            variable = robot.readline()
-            cont = 1 
-        if cont == 1:
-            variable = robot.readline()
-            cont = 2
-        varsens = variable.decode("utf-8")
-        # archivo = open("sensores.txt", "r+")
-        # archivo.write(varsens)
-        # archivo.read()
-        print(varsens)
+
     elif key == 'S' or key == 's':
         print("atras")
 
         robot.write(atras.encode())
         robot.write('\n'.encode()) 
 
-        variable = robot.readline()
-        print (variable)
     elif key == 'D' or key == 'd':
         print("derecha")
 
         robot.write(derecha.encode())
         robot.write('\n'.encode()) 
 
-        variable = robot.readline()
-        print (variable)
     elif key == 'A' or key == 'a':
         print("izquierda")
 
         robot.write(izquierda.encode())
-        robot.write('\n'.encode()) 
+        robot.write('\n'.encode())
 
-        variable = robot.readline()
-        print (variable)
     elif key == 'E' or key == 'e':
         print("Rotando hacia la derecha")
 
         robot.write(rot_der.encode())
-        robot.write('\n'.encode()) 
+        robot.write('\n'.encode())
 
-        variable = robot.readline()
-        print (variable)
     elif key == 'Q' or key == 'q':
         print("Rotando hacia la izquierda")
 
         robot.write(rot_iz.encode())
-        robot.write('\n'.encode()) 
+        robot.write('\n'.encode())
 
-        variable = robot.readline()
-        print (variable)
     else:
         print("detenido")
-        # robot.close()
+        robot.close()
         break
+# Recibimos la inforación del arduino y lo imprimimos por pantalla
+    robot.write(sensor.encode())
+    robot.write('\n'.encode())
+    # Funcion para quitar mensajes.
+    if cont == 0:
+        variable = robot.readline()
+        variable = robot.readline()
+        cont=1
+    if cont == 2:
+        variable = robot.readline()
+        cont = 1 
+    if cont == 1:
+        variable = robot.readline()
+        cont = 2
+    varsens = variable.decode("utf-8")
+
+    print(varsens)
+    sleep(1)
