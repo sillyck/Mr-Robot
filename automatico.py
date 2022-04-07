73e4b3cfd8f167336f29fefd629b7ea1e5e04b1c
EloySPAIN <yole.tres@gmail.com>
2022-04-07 23:27:24 +0200

    automatico

--- automatico.py
+++ automatico.py
@@ -1,4 +1,7 @@
+import re
+from time import sleep
 from tkinter import W
+
 import serial
 # Variables, velocidades del robot
 delante = 'cmd_vel[0.1,0,0]'
@@ -8,21 +11,33 @@ izquierda = 'cmd_vel[0,-0.1,0]'
 rot_der = 'cmd_vel[0,0,0.5]'
 rot_iz = 'cmd_vel[0,0,-0.5]'
 sensor = 'getSensors'
+cont = 0
+array_sensores_string= [0,0,0,0,0,0,0,0,0,0,0,0]
+array_sensores = [0,0,0,0,0,0,0,0,0,0,0,0]
 # Conexión a arduino
 robot = serial.Serial('/dev/ttyACM0', 115200)
 
 while True:
-    if not robot.isOpen():
-        robot.open()
+    cont=0
+    print("hola")
+    # Recibimos la inforación del arduino y lo imprimimos por pantalla
     robot.write(sensor.encode())
     robot.write('\n'.encode())
-    variable = robot.readline()
+    
+    # Funcion para quitar mensajes.
+    if cont == 0:
+        variable = robot.readline()
+        cont = 1
+    # if cont == 1:
+    #     variable = robot.readline()
+    # variable = robot.readline()
     varsens = variable.decode("utf-8")
-    # archivo = open("sensores.txt", "r+")
-    # archivo.write(varsens)
-    # archivo.read()
-    print(varsens)
-    print("adelante")
-    False
-    robot.close()
+    varsens = re.sub("Basic Encoder Test:", "0",varsens)
+    varsens = re.sub("\\r|\\n", "",varsens) #Quitamos el \r y \n que se le añade al mensaje del sensor
+    array_sensores_string=varsens.split(',') #Dividimos el mensaje y lo ponemos en el array
+    # array_sensores = [int(x) for x in array_sensores_string]
+    array_sensores = list(map(int, array_sensores_string))
+    print(array_sensores)
+    sleep(1)
+    # robot.close()
 
